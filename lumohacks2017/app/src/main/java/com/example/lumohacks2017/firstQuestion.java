4946b1c78eef1d1829d91b1887a78d83a9830713
Navjashan Singh <navjashansingh@gmail.com>
2017-09-16 16:18:43 -0700

    Sample Questions

--- lumohacks2017/app/src/main/java/com/example/lumohacks2017/firstQuestion.java
+++ lumohacks2017/app/src/main/java/com/example/lumohacks2017/firstQuestion.java
@@ -4,26 +4,27 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
-public class MainActivity extends AppCompatActivity {
+public class firstQuestion extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_first_question);
 
-        TextView hello_message = (TextView) findViewById(R.id.hello);
-        hello_message.setText("Hey Jenny!!");
+        TextView firstQuestion = (TextView) findViewById(R.id.question1);
+        firstQuestion.setText("This is your first question!!! \n Answer: YES or NO");
     }
 
-    public void sendMessage(View view) {
+    public void nextQuestion(View view) {
         Intent intent = new Intent(this, firstQuestion.class);
         intent.putExtra(EXTRA_MESSAGE, "");
         startActivity(intent);
     }
+
+
 
 }
