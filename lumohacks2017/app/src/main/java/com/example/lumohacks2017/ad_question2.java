d2e6c0430170219ac4980375ab506544d4df0b3d
Navjashan Singh <navjashansingh@gmail.com>
2017-09-17 15:03:26 -0700

    Demoable

--- lumohacks2017/app/src/main/java/com/example/lumohacks2017/ad_question2.java
+++ lumohacks2017/app/src/main/java/com/example/lumohacks2017/ad_question2.java
@@ -15,13 +15,13 @@ public class ad_question2 extends AppCompatActivity {
 
     public void yesSendMessage(View view) {
         medicalHistory.source1.put("Do you understand that physical restraints and/or sedating medications may be needed if there is an immediate danger to the patient or others around them?", "Yes, I understand this!");
-        Intent intent = new Intent(this, signAdvanced.class);
+        Intent intent = new Intent(this, summaryAdvanced.class);
         startActivity(intent);
     }
 
     public void noSendMessage(View view) {
         medicalHistory.source1.put("Do you understand that physical restraints and/or sedating medications may be needed if there is an immediate danger to the patient or others around them?", "No, I don't understand this!");
-        Intent intent = new Intent(this, signAdvanced.class);
+        Intent intent = new Intent(this, summaryAdvanced.class);
         startActivity(intent);
     }
 
